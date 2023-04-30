f5146c5f3da3834fb06c44869fa72106ce48d78e
Valik Kinakh <valik.kinah@gmail.com>
2023-04-30 12:52:34 +0300

    add calculate form

---
+++ pages/calculate.js
@@ -0,0 +1,122 @@
+import {Box, Flex, Grid, Text} from '@chakra-ui/layout';
+import {useForm} from "react-hook-form";
+import {Select, Input, Button, Heading} from "@chakra-ui/react";
+import {filterData} from "../utils/filterData";
+
+const zones = [
+    {
+        value: 1,
+        name: 'East'
+    },
+    {
+        value: 2,
+        name: 'Center'
+    },
+    {
+        value: 3,
+        name: 'West'
+    }]
+const rooms = filterData[6]?.items;
+const bathrooms = filterData[7]?.items;
+const interior = filterData.at(-2)?.items;
+const types = filterData.at(-1)?.items;
+
+const Calculate = () => {
+    const {register, handleSubmit, formState: {errors}} = useForm();
+
+    const onSubmit = (values) => {
+        console.log(values);
+    }
+
+    return <Box border='2px' borderRadius='24px' borderColor='gray.200' color='black.400' maxWidth='500px' margin='auto'
+                px='4' py='12' my='10'>
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <Heading color="blackAlpha.500" textAlign='center' as='h1' size='lg' py='2'>
+                Calculate property type form
+            </Heading>
+            <Grid templateColumns='repeat(2, 1fr)' alignItems="center" gridColumnGap="4" pt='3'>
+               <Box>
+                   <Select placeholder="Property zone" {...register('zone', {required: true})}>
+                       {zones.map((item) => (
+                           <option value={item.value} key={item.value}>
+                               {item.name}
+                           </option>
+                       ))}
+                   </Select>
+                   <Box minH='24px'>
+                       {errors?.zone && <Text color='red.400'>This field is required</Text>}
+                   </Box>
+               </Box>
+
+              <Box>
+                  <Input type="number" placeholder="Area" {...register("area", {required: true, min: 10})} />
+                  <Box minH='24px'>
+                      {errors?.area && <Text color='red.400'>This field is required</Text>}
+                  </Box>
+              </Box>
+            </Grid>
+
+            <Grid templateColumns='repeat(2, 1fr)' alignItems="center" gridColumnGap="4">
+                <Box>
+                    <Select placeholder="Number of rooms" {...register('rooms', {required: true})}>
+                        {rooms.map((item) => (
+                            <option value={item.value} key={item.value}>
+                                {item.name}
+                            </option>
+                        ))}
+                    </Select>
+                    <Box minH='24px'>
+                        {errors?.rooms && <Text color='red.400'>This field is required</Text>}
+                    </Box>
+                </Box>
+
+                <Box>
+                    <Select placeholder="Number of bathrooms" {...register('bathrooms', {required: true})}>
+                        {bathrooms.map((item) => (
+                            <option value={item.value} key={item.value}>
+                                {item.name}
+                            </option>
+                        ))}
+                    </Select>
+                    <Box minH='24px'>
+                        {errors?.bathrooms && <Text color='red.400'>This field is required</Text>}
+                    </Box>
+                </Box>
+            </Grid>
+
+            <Grid templateColumns='repeat(2, 1fr)' alignItems="center" gridColumnGap="4" py='3'>
+                <Box>
+                    <Select placeholder="Interior type" {...register('interior',{required: true})}>
+                        {interior.map((item) => (
+                            <option value={item.value} key={item.value}>
+                                {item.name}
+                            </option>
+                        ))}
+                    </Select>
+                    <Box minH='24px'>
+                        {errors?.interior && <Text color='red.400'>This field is required</Text>}
+                    </Box>
+                </Box>
+
+               <Box>
+                   <Select placeholder="Property type" {...register('type', {required: true})}>
+                       {types.map((item) => (
+                           <option value={item.value} key={item.value}>
+                               {item.name}
+                           </option>
+                       ))}
+                   </Select>
+                   <Box minH='24px'>
+                       {errors?.type && <Text color='red.400'>This field is required</Text>}
+                   </Box>
+               </Box>
+            </Grid>
+
+            <Flex justifyContent='center' mt='5'>
+                <Button colorScheme='blue' type='submit'>Calculate</Button>
+            </Flex>
+        </form>
+    </Box>
+}
+
+export default Calculate;
--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -6,7 +6,7 @@ export const fetchApi = async (url) => {
   const { data } = await axios.get((url), {
     headers: {
       'x-rapidapi-host': 'bayut.p.rapidapi.com',
-      'x-rapidapi-key': process.env.NEXT_PUBLIC_RAPID_API_KEY ,
+      'x-rapidapi-key': '22a63f9a58msh7c94a3b5f6fe364p14d285jsn416f88ae275c' ,
     },
   });
     
